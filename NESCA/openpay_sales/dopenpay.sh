a7ab87f743b41de63afc9217edd8e38c8e6487bb
Raul Morales <drag.rojo@gmail.com>
2020-06-10 15:58:51 -0500

    quitando NESCA

--- NESCA/openpay_sales/dopenpay.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-curl https://api.openpay.mx/v1/myhbbepw14sfvxyylolr/customers/aiy9foox9hfdiawd81zy/charges/tr2zvlq34biwm4cpiwug -u sk_094e464a249f470b9eba5cdc9a7664c6: > /var/www/magento/NESCA/openpay_sales/100001062.sales_order
--- NESCA/openpay_sales/dopenpay_clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm /var/www/magento/NESCA/openpay_sales/*.sales_order -f
